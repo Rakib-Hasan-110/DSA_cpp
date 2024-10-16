1fe9935296862b57e8505a5e09f066f44ab702ca
Rakib Hasan 110 <rakibhasanofficials110@gmail.com>
2024-10-16 16:34:33 +0600

    Check for balanced brackets.cpp

---
+++ Stack.cpp/Check for balanced brackets.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+bool ispar(const string& s) {  
+   
+    stack<char> stk;
+    for (int i = 0; i < s.length(); i++) {
+        
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            stk.push(s[i]); 
+        } 
+      
+        else {
+            
+            if (!stk.empty() && 
+                ((stk.top() == '(' && s[i] == ')') ||
+                 (stk.top() == '{' && s[i] == '}') ||
+                 (stk.top() == '[' && s[i] == ']'))) {
+                stk.pop(); 
+            }
+            else {
+                return false; 
+            }
+        }
+    }
+    
+    return stk.empty();
+}
+
+int main() {
+  
+    string s = "{()}[]";
+
+    if (ispar(s))
+        cout << "true";
+    else
+        cout << "false";
+    return 0;
+}
